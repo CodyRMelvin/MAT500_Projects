93d89a083dbb3376b661f9ffd671faa96891a48c
CodyRMelvin <codyrmelvin@gmail.com>
2026-01-21 02:34:36 -0800

    Can now vary number of dimensions

--- Scripts/ChartDispatcher.gd
+++ Scripts/ChartDispatcher.gd
@@ -1,9 +1,15 @@
 extends VBoxContainer
 
 @onready var spinBox: SpinBox = $ColorRect/FooterContainer_HBox/SpinBox
+@onready var chartManager: ChartManager = $ChartManager
+@onready var masterHeader: Control = $UI_MasterHeader
 
 func OnSpinBoxUpdate( value: float ) -> void:
-	print(value)
+	chartManager.RegenBernsteinCoefficients( int(value) )
+
+func OnScreenReset() -> void:
+	chartManager.ResetBernsteinCoefficients()
 
 func _ready() -> void:
 	spinBox.SpinBoxUpdate.connect(OnSpinBoxUpdate)
+	masterHeader.ClearScreen.connect(OnScreenReset)
--- Scripts/chart_manager.gd
+++ Scripts/chart_manager.gd
@@ -1,32 +1,9 @@
+class_name ChartManager
 extends Control
 
 @onready var chart: Chart = $Chart 
 
 var bCoefficients: Array[float] = [ 1.0, 1.0 ]
-
-var chartFunction: Function = Function.new(
-
-	[ 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0 ],
-	[ -3.0, -2.5, -2.0, -1.5, -1.0, -0.5, 0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0 ],
-	"Placeholder",
-	{
-		type = Function.Type.LINE,
-		marker = Function.Marker.SQUARE,
-		color = Color("#36a2eb")
-	}
-)
-
-var chartFunction2: Function = Function.new(
-
-	GenerateXValues( bCoefficients.size() - 1 ),
-	bCoefficients,
-	"Placeholder 2",
-	{
-		type = Function.Type.SCATTER,
-		marker = Function.Marker.CIRCLE,
-		color = Color( 1, 0, 0 )
-	}
-)
 
 func GenerateXValues( dim: int ) -> Array[float]:
 	var xVals: Array[float] 
@@ -36,5 +13,43 @@ func GenerateXValues( dim: int ) -> Array[float]:
 
 	return xVals
 
+func RegenBernsteinCoefficients( dim: int ) -> void:
+	bCoefficients.resize( dim + 1 )	
+	bCoefficients.fill(1.0)
+	DrawGraph()
+
+func ResetBernsteinCoefficients() -> void:
+	bCoefficients.fill(1.0)
+	DrawGraph()
+
 func _ready() -> void:
+	DrawGraph()
+
+func DrawGraph() -> void:
+
+	var chartFunction: Function = Function.new(
+
+		[ 0.0, .25, .5, .75, 1.0 ],
+		[ -3.0, -1.5, 0.0, 1.5, 3.0 ],
+		"Placeholder",
+		{
+			type = Function.Type.LINE,
+			marker = Function.Marker.SQUARE,
+			color = Color("#36a2eb")
+		}
+	)
+
+	var chartFunction2: Function = Function.new(
+
+		GenerateXValues( bCoefficients.size() - 1 ),
+		bCoefficients,
+		"Placeholder 2",
+		{
+			type = Function.Type.SCATTER,
+			marker = Function.Marker.CIRCLE,
+			color = Color( 1, 0, 0 )
+		}
+	)
+
+	chart.queue_redraw()
 	chart.plot( [ chartFunction, chartFunction2 ] )
