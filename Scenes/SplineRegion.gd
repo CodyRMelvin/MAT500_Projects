e34a91d4d5aa5ad367c56eff514d303525344691
CodyRMelvin <codyrmelvin@gmail.com>
2026-01-31 12:51:25 -0800

    Can now right click and drag points

--- Scenes/SplineRegion.gd
+++ Scenes/SplineRegion.gd
@@ -15,6 +15,8 @@ enum DrawStyle
 
 var style: DrawStyle = DrawStyle.NLI
 var points: Array[Vector2]
+var isDragging: bool = false
+var dragPointRef: int
 
 func Clear() -> void:
 	points.clear()
@@ -28,11 +30,32 @@ func _gui_input(event: InputEvent) -> void:
 		points.append( event.position )
 		queue_redraw()
 
+	if event.is_action_pressed("right click"):
+		isDragging = true
+		var distance: float = 1.79769e308
+		var mousePos: Vector2 = event.position
+		print("test")
+
+		for i: int in points.size():
+			var currentLength: float = ( points[i] - mousePos ).length()
+			
+			if currentLength < distance:
+				dragPointRef = i 
+				distance = currentLength
+
+	if event.is_action_released("right click"):
+		isDragging = false
+		
+func _process( _delta: float ) -> void:
+	if isDragging:
+		points[dragPointRef] = get_local_mouse_position()
+		queue_redraw()
+
 func _draw() -> void:
 	for point: Vector2 in points:
 		draw_circle( point, 5, Color.RED )
 
 	if style == DrawStyle.NLI:
 		for i: int in points.size() - 1:
-			draw_line( points[i], points[ i + 1 ], Color( 0, 0, 1, .5 ), 2 )
+			draw_line( points[i], points[ i + 1 ], Color( 0, 0, 1, .25 ), 2 )
 			pass
