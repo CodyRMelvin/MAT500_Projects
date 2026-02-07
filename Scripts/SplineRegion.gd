94dcafb52a4eff35611593e38159c1accbbdbda0
CodyRMelvin <codyrmelvin@gmail.com>
2026-02-06 19:19:51 -0800

    Implemented bbform, started midpoint subdivision

--- Scripts/SplineRegion.gd
+++ Scripts/SplineRegion.gd
@@ -28,6 +28,7 @@ var splinePoints: Array[Vector2]
 var isDragging: bool = false
 var dragPointRef: int
 var t: float = 0.5
+const msDegree: int = 3
 
 func Clear() -> void:
 	points.clear()
@@ -82,7 +83,6 @@ func DrawNLI() -> void:
 	
 	draw_circle( shellPoints.back()[0], pointRadius, pointColor )
 
-	splinePoints.clear()
 	splinePoints.append( points[0] )
 	var step: float = 1 / float(granularity)
 	var ti: float = step
@@ -104,12 +104,82 @@ func DrawNLI() -> void:
 		ti += step
 
 	splinePoints.append( points.back() )
+
+func Factorial( i: float ) -> float:
+	if i < 2:
+		return 1
+	return i * Factorial( i - 1 );
 	
+func Choose( d: float, i: float ) -> float:
+	if d == 0 || i == 0 || d == i:
+		return 1
+	return Factorial(d) / ( Factorial(i) * Factorial( d - i ) ) 
+
+func Bernstein( d: float, i: float, t: float ) -> float:
+	var choose = Choose( d, i )
+	return choose * pow( t, i ) * pow( 1 - t, d - i )
+
 func DrawBBForm() -> void:
-	pass
+	if points.size() < 2:
+		return
+
+	splinePoints.append( points[0] )
+	var step: float = 1 / float(granularity)
+
+	for ti: int in range( 1, granularity ):
+		var pointBuffer: Vector2 = Vector2.ZERO
+
+		for i: float in points.size():
+			pointBuffer += points[i] * Bernstein( points.size() - 1, i, ti * step )
+
+		splinePoints.append(pointBuffer)		
+	
+	splinePoints.append( points.back() )
+
+func InterpolatePoint( pointArray: Array[Vector2], d: int, i: int, t: float ) -> Vector2:
+	if pointArray.size() < 1:
+		return Vector2.ZERO
+
+	if d < 0 || i < 0:
+		return Vector2.ZERO
+
+	if pointArray.size() < d + i:
+		return Vector2.ZERO
+
+	if d == 0:
+		return pointArray[i]
+
+	var pointBuffer1: Array[Vector2]
+	pointBuffer1.append_array(pointArray)
+
+	for j: int in d:
+		if pointBuffer1.size() < 2:
+			break
+
+		var pointBuffer2: Array[Vector2]
+
+		for k: int in range( i, pointBuffer1.size() - 1 ):
+			pointBuffer2.append( lerp( pointBuffer1[k], pointBuffer1[ k + 1 ], .5 ) )
+
+		pointBuffer1.clear()
+		pointBuffer1.append_array(pointBuffer2)
+
+	return pointBuffer1[i]
+	
 	
 func DrawMS() -> void:
-	pass
+	if points.size() < 2:
+		return
+
+	if msDegree < 1:
+		return
+
+	if msDegree == 1:
+		splinePoints = points
+		return
+	
+
+	
 
 func _ready() -> void:
 	masterHeader.connect( "ClearScreen", Clear )
@@ -139,6 +209,8 @@ func _process( _delta: float ) -> void:
 		queue_redraw()
 
 func _draw() -> void:
+	splinePoints.clear()
+
 	match style:
 		DrawStyle.NLI:
 			DrawNLI()
@@ -151,6 +223,7 @@ func _draw() -> void:
 
 	for i: int in splinePoints.size() - 1:
 		draw_line( splinePoints[i], splinePoints[ i + 1 ], splineColor, splineWidth )
+
 
 	for point: Vector2 in points:
 		draw_circle( point, pointRadius, pointColor )
