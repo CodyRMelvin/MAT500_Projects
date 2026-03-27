5b994bcb605bb6f4c43681de5258531403f5f636
CodyRMelvin <codyrmelvin@gmail.com>
2026-03-27 13:58:41 -0700

    Set up project 4 and cleaned up project 3

--- Scenes/UI_MasterHeader_ProjectButton.gd
+++ Scenes/UI_MasterHeader_ProjectButton.gd
@@ -2,6 +2,7 @@ extends MenuButton
 
 var project2Scene = "res://Scenes/Project_2.tscn"
 var project3Scene = "res://Scenes/Project_3.tscn"
+var project4Scene = "res://Scenes/Project_4.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,3 +15,5 @@ func _on_pressed( id: int ) -> void:
 			get_tree().change_scene_to_file(project2Scene)
 		3:
 			get_tree().change_scene_to_file(project3Scene)
+		4:
+			get_tree().change_scene_to_file(project4Scene)
